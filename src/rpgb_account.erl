8d6f4fd35d8f8e37204d43400180e6f17beec87e
Micah Warren <micahw@lordnull.com>
2012-02-24 01:28:49 -0600

    Removed session handling (as rpgb_session does that)

--- src/rpgb_account.erl
+++ src/rpgb_account.erl
@@ -41,31 +41,47 @@ allowed_methods(ReqData, Ctx) ->
 %	{true, ReqData, Ctx}.
 	
 process_post(ReqData, Ctx) ->
+	?info("processing a likely login"),
 	Post = mochiweb_util:parse_qs(wrq:req_body(ReqData)),
 	Openid = proplists:get_value("openid", Post, ""),
 	?info("Openid:  ~p", [Openid]),
-	SessionId0 = wrq:get_cookie_value("rpgbsid", ReqData),
-	{ok, Session} = rpgb_session:get_or_create(SessionId0),
-	SessionId1 = rpgb_session:get_id(Session),
-	{CookieHKey, CookieHVal} = mochiweb_cookies:cookie("rpgbsid", SessionId1, [{max_age, 60 * 60 * 24 * 7}]),
-	ReqData0 = wrq:set_resp_header(CookieHKey, CookieHVal, ReqData),
-	case gen_server:call(openid, {prepare, SessionId1, Openid, true}) of
+	{ok, Session} = rpgb_session:get(ReqData),
+	SessionId = rpgb_session:get_id(Session),
+	case gen_server:call(openid, {prepare, SessionId, Openid, true}) of
 		{ok, AuthReq} ->
 			BaseURL = rpg_battlemap_app:get_url(),
 			ReturnTo = <<BaseURL/binary, "/account/login_complete">>,
 			AuthURL = openid:authentication_url(AuthReq, ReturnTo, BaseURL),
-			ReqData1 = wrq:set_resp_header("Location", binary_to_list(AuthURL), ReqData0),
-			ReqData2 = wrq:do_redirect(true, ReqData1),
-			{true, ReqData2, Ctx};
+			ReqData0 = wrq:set_resp_header("Location", binary_to_list(AuthURL), ReqData),
+			ReqData1 = wrq:do_redirect(true, ReqData0),
+			{true, ReqData1, Ctx};
 		{error, Err} ->
 			?info("Error from openid:  ~p", [Err]),
-			{false, ReqData0, Ctx}
+			{false, ReqData, Ctx}
 	end.
 
 to_html(ReqData, Ctx) ->
 	?info("to html"),
+	ReqData1 = case wrq:path_info(action, ReqData) of
+		"login" ->
+			{ok, _Session, ReqData0} = rpgb_session:get_or_create(ReqData),
+			ReqData0;
+		"login_complete" ->
+			{ok, Session, ReqData0} = rpgb_session:get_or_create(ReqData),
+			SessionId = rpgb_session:get_id(Session),
+			BaseURL = rpg_battlemap_app:get_url(),
+			ReturnTo = <<BaseURL/binary, "/account/login_complete">>,
+			QueryParams = wrq:req_qs(ReqData),
+			case gen_server:call(openid, {verify, SessionId, ReturnTo, QueryParams}) of
+				{ok, UserId} ->
+					io:format("We have ourselves a user:  ~p", [UserId]);
+				{error, Fail} ->
+					io:format("And it's all fucked up:  ~p", [Fail])
+			end,
+			ReqData0
+	end,
 	{ok, Out} = base_dtl:render([]),
-	{Out, ReqData, Ctx}.
+	{Out, ReqData1, Ctx}.
 
 %to_json(ReqData, Ctx) ->
 %	?info("to json"),
