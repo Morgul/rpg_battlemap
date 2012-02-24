681ff60b1088b5acb1c87ce41b9b17befb60c62f
Micah Warren <micahw@lordnull.com>
2012-02-23 23:46:18 -0600

    Added helpful helper functions of helping.

--- src/rpg_battlemap_app.erl
+++ src/rpg_battlemap_app.erl
@@ -4,6 +4,8 @@
 
 %% Application callbacks
 -export([start/2, stop/1]).
+%% api
+-exprt([get_env/1, get_env/2, get_url/0]).
 
 %% ===================================================================
 %% Application callbacks
@@ -14,3 +16,23 @@ start(_StartType, _StartArgs) ->
 
 stop(_State) ->
     ok.
+
+
+%% ===================================================================
+%% Utilties
+%% ===================================================================
+
+get_env(Key) ->
+	application:get_env(rpg_battlemap, Key).
+
+get_env(Key, Default) ->
+	case application:get_env(Key) of
+		undefined -> {ok, Default};
+		E -> E
+	end.
+
+get_url() ->
+	{ok, Host} = get_env(host, "localhost"),
+	{ok, Port} = get_env(port, 9090),
+	{ok, Proto} = get_env(protocol, http),
+	iolist_to_binary(io_lib:format("~s://~s:~p", [Proto,Host, Port])).
