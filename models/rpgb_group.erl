59e97eb742fbc0b71effe1b8a764434834145c50
Micah Warren <micahw@lordnull.com>
2012-02-26 17:22:29 -0600

    Hooks for tracking update on groups too now.

--- models/rpgb_group.erl
+++ models/rpgb_group.erl
@@ -1,2 +1,9 @@
--module(rpgb_group, [Id, Name::binary(), CreatedTime::timestamp(), UpdatesTime::timestamp()]).
+-module(rpgb_group, [Id, Name::binary(), CreatedTime::timestamp(), UpdatedTime::timestamp()]).
 -has({permissions, many}).
+-compile([export_all]).
+
+before_create() ->
+	{ok, ?MODULE:new(Id, Name, erlang:now(), erlang:now())}.
+
+before_update() ->
+	{ok, ?MODULE:new(Id, Name, CreatedTime, erlang:now())}.
