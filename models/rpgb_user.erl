7bf6ecaf64962a411850dc9f5d5e9397a1cb282f
Micah Warren <micahw@lordnull.com>
2012-02-26 17:13:55 -0600

    Added hooks for tracking last update.

--- models/rpgb_user.erl
+++ models/rpgb_user.erl
@@ -1,3 +1,10 @@
--module(rpgb_user, [Id, Name, OpenID, RpgbGroupId, CreateTime, UpdateTime]).
+-module(rpgb_user, [Id, Name::binary(), OpenID::binary(), RpgbGroupId, CreatedTime::timestamp(), UpdatedTime::timestamp()]).
 -has({permissions, many, [{module, rpgb_permission}]}).
 -belongs_to(rpgb_group).
+-compile([export_all]).
+
+before_create() ->
+	{ok, ?MODULE:new(Id, Name, OpenID, RpgbGroupId, erlang:now(), erlang:now())}.
+
+before_update() ->
+	{ok, ?MODULE:new(Id, Name, OpenID, RpgbGroupId, CreatedTime, erlang:now())}.
