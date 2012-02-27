7f8168a70201293ecfa4a20a86f60159147b4e90
Micah Warren <micahw@lordnull.com>
2012-02-27 00:46:00 -0600

    Added starting of test node to testy things.

--- src/rpgb_util.erl
+++ src/rpgb_util.erl
@@ -1,5 +1,8 @@
 -module(rpgb_util).
 -export([hexstr_to_bin/1, bin_to_hexstr/1]).
+% functions to help set up testing
+-export([start_testnode/0, start_testnode/1, start_testnode/2, add_paths/0,
+	cover_mods/1]).
 
 %% @doc Converts binaries to hexidecimal strings.
 -spec(bin_to_hexstr/1 :: (Bin :: binary()) -> [48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 97 | 98 | 99 | 100 | 101 | 102]).
@@ -25,3 +28,49 @@ hexstr_to_bin([X, Y | T], Acc) ->
 		_Else ->
 			error
 	end.
+
+start_testnode() ->
+	add_paths(),
+	case node() of
+		nonode@nohost ->
+			[] = os:cmd("epmd -daemon"),
+			case net_kernel:start([rpg_battlemap_test, shortnames]) of
+				{ok, _} ->
+					node();
+				{error, {{already_started, _}, _}}  ->
+					node();
+				_ ->
+					erlang:error(node_fail)
+			end;
+		Else ->
+			Else
+	end.
+
+start_testnode(Name) ->
+	start_testnode(Name, net_adm:localhost()).
+
+start_testnode(Name, Host) ->
+	start_testnode(),
+	case slave:start_link(Host, Name) of
+		{ok, N} -> 
+			rpc:call(N, ?MODULE, add_paths, []),
+			Covered = [Mod || {Mod, cover_compiled} <- code:all_loaded()],
+			rpc:call(N, ?MODULE, cover_mods, [Covered]),
+			N;
+		{error, {already_running, N}} -> N
+	end.
+
+add_paths() ->
+	{Pre, Deps} = case {file:list_dir("deps"), file:list_dir("../deps")} of
+		{{ok, Files}, _} -> {"deps/", Files};
+		{_, {ok, Files}} -> {"../deps/", Files};
+		_ -> {[], []}
+	end,
+	Paths = [Pre ++ X ++ "/ebin" || X <- Deps],
+	code:add_paths(Paths).
+
+cover_mods([]) ->
+	cover:start();
+cover_mods([Mod | Tail]) ->
+	{ok, Mod} = cover:compile_beam(Mod),
+	cover_mods(Tail).
