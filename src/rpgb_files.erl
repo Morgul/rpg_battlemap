c6e5ce80521075071e9491442f2e952db89b6e0d
Micah Warren <micahw@lordnull.com>
2012-03-23 17:36:29 -0500

    Checking for existing files before serving is a good thing.

--- src/rpgb_files.erl
+++ src/rpgb_files.erl
@@ -1,12 +1,19 @@
 -module(rpgb_files).
 
--export([init/1,to_html/2]).
+-export([init/1,to_html/2,resource_exists/2]).
 -include_lib("webmachine/include/webmachine.hrl").
 -include("log.hrl").
 
 
 init(Args) ->
 	{ok, undefined}.
+
+resource_exists(ReqData, Context) ->
+	Path = case wrq:path(ReqData) of
+		"/" -> "index.html";
+		X -> X
+	end,
+	{filelib:is_file("priv/www" ++ Path), ReqData, Context}.
 
 to_html(ReqData, Context) ->
 	Path = case wrq:path(ReqData) of
