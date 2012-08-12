a58cc2334d73ea36c9a104f20d6b2bf8d0d492cd
Micah Warren <micahw@lordnull.com>
2012-08-11 23:47:38 -0500

    Removing boss_db models.

--- models/rpgb_group.erl
+++
@@ -1,9 +0,0 @@
--module(rpgb_group, [Id, Name::binary(), CreatedTime::timestamp(), UpdatedTime::timestamp()]).
--has({permissions, many}).
--compile([export_all]).
-
-before_create() ->
-	{ok, ?MODULE:new(Id, Name, erlang:now(), erlang:now())}.
-
-before_update() ->
-	{ok, ?MODULE:new(Id, Name, CreatedTime, erlang:now())}.
