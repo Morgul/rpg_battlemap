66ffb054ab0d32952cffd47a2e5742ef340b2a0d
Micah Warren <micahw@lordnull.com>
2012-09-29 23:42:03 -0500

    Removing modules meant for boss_db.

--- app/rpg_battlemap/src/user_group.erl
+++
@@ -1,9 +0,0 @@
--module(user_group, [Id, Name, Permissions, Created, Updated]).
-
--export([id/0, name/0, permissions/0, created/0, updated/0]).
-
-id() -> Id.
-name() -> Name.
-permissions() -> Permissions.
-created() -> Created.
-updated() -> Updated.
