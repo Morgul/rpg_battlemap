e6d86f3404130ffd559e5efae8f9a692094e0344
Micah Warren <micahw@lordnull.com>
2012-11-01 18:21:58 -0500

    nixxing debug log output; more than useful

--- app/rpg_battlemap/include/log.hrl
+++ app/rpg_battlemap/include/log.hrl
@@ -1,35 +1,35 @@
 %% because the loggers rarely include everything I'd like to see,
 %% this wraps 'em up.
 
--ifdef(TEST).
-
--include_lib("eunit/include/eunit.hrl").
-
--define(debug(Message), ?debugFmt("[~p][~p][~p]~n	DEBUG: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(debug(Message, Args), ?debugFmt("[~p][~p][~p]~n	DEBUG: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(info(Message), ?debugFmt("[~p][~p][~p]~n	INFO: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(info(Message, Args), ?debugFmt("[~p][~p][~p]~n	INFO: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(notice(Message), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(notice(Message, Args), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(warning(Message), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(warning(Message, Args), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(error(Message), ?debugFmt("[~p][~p][~p]~n	ERROR: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(error(Message, Args), ?debugFmt("[~p][~p][~p]~n	ERROR: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(critical(Message), ?debugFmt("[~p][~p][~p]~n	CRITICAL: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(critical(Message, Args), ?debugFmt("[~p][~p][~p]~n	CRITICAL: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(alert(Message), ?debugFmt("[~p][~p][~p]~n	ALERT: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(alert(Message, Args), ?debugFmt("[~p][~p][~p]~n	ALERT: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--define(emergency(Message), ?debugFmt("[~p][~p][~p]~n	EMERGENCY: ~s~n", [erlang:localtime(), node(), self(), Message])).
--define(emergency(Message, Args), ?debugFmt("[~p][~p][~p]~n	EMERGENCY: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
-
--else.
+%-ifdef(TEST).
+%
+%-include_lib("eunit/include/eunit.hrl").
+%
+%-define(debug(Message), ?debugFmt("[~p][~p][~p]~n	DEBUG: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(debug(Message, Args), ?debugFmt("[~p][~p][~p]~n	DEBUG: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(info(Message), ?debugFmt("[~p][~p][~p]~n	INFO: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(info(Message, Args), ?debugFmt("[~p][~p][~p]~n	INFO: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(notice(Message), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(notice(Message, Args), ?debugFmt("[~p][~p][~p]~n	NOTICE: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(warning(Message), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(warning(Message, Args), ?debugFmt("[~p][~p][~p]~n	WARNING: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(error(Message), ?debugFmt("[~p][~p][~p]~n	ERROR: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(error(Message, Args), ?debugFmt("[~p][~p][~p]~n	ERROR: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(critical(Message), ?debugFmt("[~p][~p][~p]~n	CRITICAL: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(critical(Message, Args), ?debugFmt("[~p][~p][~p]~n	CRITICAL: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(alert(Message), ?debugFmt("[~p][~p][~p]~n	ALERT: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(alert(Message, Args), ?debugFmt("[~p][~p][~p]~n	ALERT: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-define(emergency(Message), ?debugFmt("[~p][~p][~p]~n	EMERGENCY: ~s~n", [erlang:localtime(), node(), self(), Message])).
+%-define(emergency(Message, Args), ?debugFmt("[~p][~p][~p]~n	EMERGENCY: ~s~n", [erlang:localtime(), node(), self(), lists:flatten(io_lib:format(Message, Args))])).
+%
+%-else.
 
 -compile([{parse_transform, lager_transform}]).
 
@@ -57,4 +57,4 @@
 -define(emergency(Message), lager:emergency(Message)).
 -define(emergency(Message, Args), lager:emergency(Message, Args)).
 
--endif.
+%-endif.
