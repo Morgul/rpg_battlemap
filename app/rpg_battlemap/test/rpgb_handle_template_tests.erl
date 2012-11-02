7998a38304b6caaacabc86cd040da04b6b0d7961
Micah Warren <micahw@lordnull.com>
2012-11-01 22:04:02 -0500

    Fixed tests

--- app/rpg_battlemap/test/rpgb_handle_template_tests.erl
+++ app/rpg_battlemap/test/rpgb_handle_template_tests.erl
@@ -6,10 +6,10 @@
 request_test_() ->
 	{setup, fun() ->
 		application:start(cowboy),
-		HostPort = {<<"localhost">>, 9093},
+		HostPort = {<<"localhost">>, 9094},
 		meck:new(openid),
 		cowboy:start_listener(handle_template_tests, 1,
-			cowboy_tcp_transport, [{port, 9093}],
+			cowboy_tcp_transport, [{port, 9094}],
 			cowboy_http_protocol, [{dispatch, [
 				{'_', [
 					{[], rpgb_handle_template, {HostPort, index_dtl}}
@@ -17,8 +17,7 @@ request_test_() ->
 			]}]
 		),
 		ibrowse:start(),
-		rpgb_test_util:mecked_data(handle_template_data),
-		rpgb_session:make_ets()
+		rpgb_test_util:mecked_data(handle_template_data)
 	end,
 	fun(_) ->
 		meck:unload(rpgb_data),
@@ -27,13 +26,16 @@ request_test_() ->
 	fun(_) -> [
 
 		{"access without login", fun() ->
-			{ok, "200", _Heads, Body} = ibrowse:send_req("http://localhost:9093/", [], get),
-			{ok, Regex} = re:compile("action=\"http://localhost:9093/account\""),
+			?debugFmt("The sessions:  ~p", [ets:match(rpgb_session, '$1')]),
+			rpgb_session:make_ets(),
+			{ok, "200", _Heads, Body} = ibrowse:send_req("http://localhost:9094/", [], get),
+			{ok, Regex} = re:compile("action=\"http://localhost:9094/account\""),
 			?assertMatch({match, [{_,_}]}, re:run(Body, Regex))
 		end},
 
 		{"access while logged in", fun() ->
-			{ok, "200", Heads, _Body} = ibrowse:send_req("http://localhost:9093/", [], get),
+			rpgb_session:make_ets(),
+			{ok, "200", Heads, _Body} = ibrowse:send_req("http://localhost:9094/", [], get),
 			Cookie = proplists:get_value("Set-Cookie", Heads),
 			Key = ets:first(rpgb_session),
 			[Session] = ets:lookup(rpgb_session, Key),
@@ -42,7 +44,9 @@ request_test_() ->
 				name = <<"Batman">>
 			},
 			rpgb_session:set_user(User, Session),
-			{ok, "200", _Heads, Body} = ibrowse:send_req("http://localhost:9093/", [{"Cookie", Cookie}], get),
+			?debugFmt("The sessions:  ~p", [ets:match(rpgb_session, '$1')]),
+			{ok, "200", _Heads, Body} = ibrowse:send_req("http://localhost:9094/", [{"Cookie", Cookie}], get),
+			?debugFmt("The body:  ~p", [Body]),
 			{ok, Regex} = re:compile("href=\"/account/logout/\""),
 			?assertMatch({match, [{_,_}]}, re:run(Body, Regex))
 		end}
