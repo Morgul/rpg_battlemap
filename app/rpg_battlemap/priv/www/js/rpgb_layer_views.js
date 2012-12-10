59e8e114024ffbbf60f03bb3ebf2d2eff2e4e3af
Micah Warren <micahw@lordnull.com>
2012-12-10 02:53:23 -0600

    Layer visibility and selected set.

--- app/rpg_battlemap/priv/www/js/rpgb_layer_views.js
+++ app/rpg_battlemap/priv/www/js/rpgb_layer_views.js
@@ -1,21 +1,26 @@
 Ember.TEMPLATES['layerList'] = Ember.Handlebars.compile(
 '<h3 {{ action "toggleShowLayers" }}>Layers</h3>' +
-'<span {{bindStyle display="showLayers"}} class="toolbarDropdown">' +
+'<span {{bindStyle display="showLayers"}} class="toolbarDropdown layerDropDown">' +
 
 '{{#each content.topToBottom}}' +
-	'{{view RPGB.LayerListItemView tagName="p"}}' +
+	'{{view RPGB.LayerListItemView tagName="p" class="layerItem"}}' +
 '{{/each}}' +
 
 '<p>' +
 	'{{view RPGB.LayerNameField valueBinding="newName"}} <button {{action "createLayer"}}>+</button>' +
 '</p>' +
 
+'<p>' +
+	'<button {{action "deleteSelectedLayer"}}>Delete Selected</button>' +
+'</p>' +
+
 '</span>');
 
 
 Ember.TEMPLATES['layerListItem'] = Ember.Handlebars.compile(
-	'<div {{bindAttr class="visibility"}}></div>' +
-	'{{ name }}');
+	'<button {{bindAttr class="visibility"}} class="layerVisibility" {{action "cycleVisibility" }}></button>' +
+	'{{ name }}'
+);
 
 RPGB.LayerListView = Ember.View.extend({
 	templateName: 'layerList',
@@ -36,13 +41,45 @@ RPGB.LayerListView = Ember.View.extend({
 			'name': this.get('newName')
 		};
 		this.get('content').createLayer(layerArgs);
+	},
+
+	setVisibilityMode: function(mode){
+		console.log('set visibility mode', mode);
+	},
+
+	setSelected: function(layerObj){
+		this.set('content.selected', layerObj);
 	}
 });
 
 RPGB.LayerListItemView = Ember.View.extend({
 	templateName: 'layerListItem',
-	content: null,
-	visibility: 'full'
+	classNameBindings: 'context.selected',
+	//content: null,
+
+	// event handlers
+	click: function(){
+		this.set('parentView.content.selected', this.get('context'));
+		return false;
+	},
+
+	// actions
+	cycleVisibility: function(){
+		this.set('parentView.content.visibilityMode', 'custom');
+		var visibility = this.get('context.visibility');
+		switch(visibility){
+			case "full":
+				visibility = "half";
+				break;
+			case "half":
+				visibility = "none";
+				break;
+			default:
+				visibility = "full";
+		}
+		this.set('context.visibility', visibility);
+		return false;
+	}
 });
 
 RPGB.LayerNameField = Ember.TextField.extend({
@@ -50,4 +87,4 @@ RPGB.LayerNameField = Ember.TextField.extend({
 		this.set('newName', evt.target.value);
 		return false;
 	}
-})
+});
