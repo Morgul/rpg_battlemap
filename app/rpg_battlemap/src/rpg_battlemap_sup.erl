8265e76265c8e2844f04e2c00d1d5b7162cb0bf9
Micah Warren <micahw@lordnull.com>
2013-01-05 01:03:29 -0600

    Removed commented out code.

--- app/rpg_battlemap/src/rpg_battlemap_sup.erl
+++ app/rpg_battlemap/src/rpg_battlemap_sup.erl
@@ -42,31 +42,6 @@ init(Args) ->
 		Dispatch = [
 			{ListenHost, Routes}
 		],
-%    Dispatch = [
-%			{ListenHost, [
-%				{[<<"maps">>], rpgb_handle_maps, HP},
-%				{[<<"maps">>, map_id], rpgb_handle_map, HP},
-%				{[<<"maps">>, map_id, property], rpgb_handle_map, {host, Port}},
-%				{[<<"users">>], rpgb_handle_users, HP},
-%				{[<<"users">>, user_id], rpgb_handle_user, HP},
-%				{[<<"users">>, user_id, property], rpgb_handle_user, HP},
-%				{[<<"layers">>, layer_id], rpgb_handle_layer, HP},
-%				{[<<"layers">>, layer_id, property], rpgb_handle_layer, HP},
-%				{[<<"zones">>, zone_id], rpgb_handle_zone, HP},
-%				{[<<"zones">>, zone_id, property], rpgb_handle_zone, HP},
-%				{[<<"characters">>], rpgb_handle_characters, HP},
-%				{[<<"characters">>, id], rpgb_handle_character, HP},
-%				{[<<"characters">>, id, property], rpgb_handle_character, HP},
-%				{[<<"combatants">>, id], rpgb_handle_combatants, HP},
-%				{[<<"combatants">>, id, property], rpgb_handle_combatants, HP},
-%				{[<<"account">>], rpgb_handle_account, HP},
-%				{[<<"account">>, <<"login_complete">>], rpgb_handle_account, HP},
-%				{[<<"map">>], rpgb_handle_map, HP},
-%				{[<<"map">>, mapid], rpgb_handle_map, HP},
-%				{[], rpgb_handle_template, {HP, index_dtl}},
-%				{'_', rpgb_handle_default, HP}
-%			]}
-%		],
 
 		cowboy:start_listener(rpgb_listener, Listeners, 
 			cowboy_ssl_transport, [{keyfile, Keyfile}, {certfile, Certfile},
