621f5a61ab8af959aa8b392b87b67a463b33e12f
Micah Warren <micahw@lordnull.com>
2013-02-08 15:42:47 -0600

    Zone view start.

---
+++ app/rpg_battlemap/priv/www/js/rpgb_zone_views.js
@@ -0,0 +1,100 @@
+Ember.TEMPLATES['zoneAuraList'] = Ember.Handlebars.compile(
+	'<div class="btn-group" {{bindStyle display="displayZoneAuraDropdown"}}>' +
+		'<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">' +
+			'Zones &amp; Auras' +
+			'<span class="caret"></span>' +
+		'</a>' +
+
+		'<div class="dropdown-menu">' +
+			'<p>Zones</p>' +
+			'{{#each content.selected.zones}}' +
+				'{{view RPGB.ZoneAuraListItemView tagName="p" class="zoneAuraItem"}}' +
+			'{{/each}}' +
+
+			'<p>' +
+				'{{view Ember.TextField valueBinding="newZoneName"}}' +
+				'<a href="#" role="button" class="btn" {{action "createNewLineZone"}}>Lines</a>' +
+				'<a href="#" role="button" class="btn" {{action "createNewFilledZone"}}>Filled</a>' +
+			'</p>' +
+
+			'<p class="divider"></p>' +
+
+			'<p>Auras</p>' +
+			'{{#each content.selected.auras}}' +
+				'{{view RPGB.ZoneAuraListItemView tagName="p" class="zoneAuraItem"}}' +
+			'{{/each}}' +
+
+			'<p>' +
+				'{{view Ember.TextField valueBinding="newAuraName"}}' +
+				'<a href="#" role="button" class="btn" {{action "createNewLineAura"}}>Lines</a>' +
+				'<a href="#" role="button" class="btn" {{action "createNewFilledAura"}}>Filled</a>' +
+			'</p>' +
+
+			'<p class="divider"></p>' +
+
+			'<p>' +
+				'<a href="#" role="button" class="btn" {{action "deleteSelected"}}>Delete</a>' +
+			'</p>' +
+		'</div>' +
+	'</div>');
+
+Ember.TEMPLATES['zoneAuraListItem'] = Ember.Handlebars.compile(
+	'{{name}}');
+
+RPGB.ZoneAuraListView = Ember.View.extend({
+	templateName: 'zoneAuraList',
+	content: null,
+	newZoneName: '',
+	newAuraName: '',
+
+	displayZoneAuraDropdown: function(){
+		var selected = this.get('content.selected');
+		if(selected){
+			return 'inline-block';
+		}
+		return 'none';
+	}.property('content.selected'),
+
+	didInsertElement: function(){
+		this.$('input, selected').on('click', function(ev){
+			ev.stopPropagation();
+		});
+	},
+
+	createNewLineZone: function(ev){
+		var name = this.get('newZoneName');
+		this.createZoneAura('zone', 'polyline', name);
+	},
+
+	createNewFilledZone: function(){
+		var name = this.get('newZoneName');
+		this.createZoneAura('zone', 'polygon', name);
+	},
+
+	createNewLineAura: function(){
+		var name = this.get('newAuraName');
+		this.createZoneAura('aura', 'polyline', name);
+	},
+
+	createNewFilledAura: function(){
+		var name = this.get('newAuraName');
+		this.createZoneAura('aura', 'polygon', name);
+	},
+
+	createZoneAura: function(type, element, name){
+		var selected = this.get('content.selected');
+		if(! selected){
+			return;
+		}
+		selected.createZoneAura(type, element, name);
+	}
+});
+
+RPGB.ZoneAuraListItemView = Ember.View.extend({
+	templateName: 'zoneAuraListItem',
+	classNameBindings: 'context.selected',
+
+	click: function(){
+		this.set('parentView.content.selected.selectedZoneAura', this.get('context'));
+	}
+})
